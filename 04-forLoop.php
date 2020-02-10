4c3094a8ad88aaea690cfbef1aad29fc2e56bcfe
Rachel <rac777blue@hotmail.com>
2020-02-10 11:50:56 +0000

    completed challenge 4

---
+++ 04-forLoop.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i = 13; $i < 1000; $i += 13) {
+    var_dump($i);
+}
+
+// Write some code that lists all multiples of 13 up to 1,000 using a for loop
+
+// Example output:
+
+// int(13)
+// int(26)
+// int(39)
+// int(52)
+// …
+// int(988)
