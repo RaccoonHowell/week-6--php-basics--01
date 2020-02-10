6edf844831d051a84443ba893a6d8a46de408644
Rachel <rac777blue@hotmail.com>
2020-02-10 11:58:29 +0000

    completed challenge 5

---
+++ 05-even.php
@@ -0,0 +1,18 @@
+<?php
+
+for($i = 2; $i <= 50; $i += 2) {
+    if($i % 3 === 0) {
+        var_dump($i);
+    }
+}
+
+// <!-- Write some code that outputs any even numbers between 1 and 50 that are also divisible by 3
+
+// Example output:
+
+// int(6)
+// int(12)
+// int(18)
+// int(24)
+// …
+// int(48) -->
